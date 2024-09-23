393540045e495ba952af4d281bc8288dac7a575d
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2024-09-24 03:23:41 +0600

    added banner and signpu

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,93 +1,136 @@
 import React from "react";
+import { Form, Input, Button } from "antd";
+import { UserOutlined, MailOutlined, LockOutlined } from "@ant-design/icons";
+import signupImage from "../assets/signup.png";
+import { Link } from "react-router-dom";
 
-function Signup() {
+const Signup = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
   return (
-    <div>
-      <div className="hero ">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center hidden md:block lg:text-left">
-            <h1 className="text-5xl font-bold">Sign up now!</h1>
-            <p className="py-6">
-              Join Bright Future IT by registering today and take the first step
-              toward mastering Flutter development. Registration gives you
-              access to our comprehensive courses, live sessions, and hands-on
-              projects. Start your learning journey with us and unlock your
-              potential in the world of cross-platform app development!
-            </p>
-          </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Name</span>
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Your Name"
-                  className="input input-sm md:input-md input-bordered focus:outline-none focus:ring-2 focus:ring-[#884AFF] focus:border-transparent transition-transform transform duration-300 ease-in-out hover:scale-105"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="email"
-                  className="input input-sm md:input-md input-bordered focus:outline-none focus:ring-2 focus:ring-[#884AFF] focus:border-transparent transition-transform transform duration-300 ease-in-out hover:scale-105"
-                  required
-                />
-              </div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex w-full max-w-5xl bg-white rounded-lg shadow-lg overflow-hidden">
+        {/* Left Side: Image or Content */}
+        <div className="w-1/2 hidden md:block">
+          <img
+            src={signupImage}
+            alt="Signup Side Content"
+            className="object-cover h-full w-full"
+          />
+        </div>
 
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="password"
-                  className="input input-sm md:input-md input-bordered focus:outline-none focus:ring-2 focus:ring-[#884AFF] focus:border-transparent transition-transform transform duration-300 ease-in-out hover:scale-105"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Confirm Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="confirm password"
-                  className="input input-sm md:input-md input-bordered focus:outline-none focus:ring-2 focus:ring-[#884AFF] focus:border-transparent transition-transform transform duration-300 ease-in-out hover:scale-105"
-                  required
-                />
-              </div>
-              <div className="form-control mt-2">
-                <button
-                  type="submit"
-                  className="btn btn-primary btn-sm md:btn-md  text-white bg-gradient-to-r from-[#884AFF] to-[#C54AFF] hover:from-teal-400 hover:to-blue-500"
-                >
-                  Sign Up
-                </button>
-              </div>
-              <div className="md:flex justify-center items-center  md:space-x-2 mt-4">
-                <span>Already have an account?</span>{" "}
-                <a
-                  href="/login"
-                  className="text-[#884AFF] hover:text-[#C54AFF] font-normal md:font-semibold underline"
-                >
-                  Login Here
-                </a>
-              </div>
-            </form>
-          </div>
+        {/* Right Side: Signup Form */}
+        <div className="w-full md:w-1/2 p-8">
+          <h2 className="text-center text-3xl font-bold text-gray-800 mb-6">
+            Create a New Account
+          </h2>
+
+          <Form
+            name="signup"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            layout="vertical"
+          >
+            {/* Username Field */}
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+              label="Username"
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Username"
+                className="rounded-lg"
+              />
+            </Form.Item>
+
+            {/* Email Field */}
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input a valid email!",
+                  type: "email",
+                },
+              ]}
+              label="Email"
+            >
+              <Input
+                prefix={<MailOutlined className="site-form-item-icon" />}
+                placeholder="Email"
+                className="rounded-lg"
+              />
+            </Form.Item>
+
+            {/* Password Field */}
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+              label="Password"
+            >
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                placeholder="Password"
+                className="rounded-lg"
+              />
+            </Form.Item>
+
+            {/* Confirm Password Field */}
+            <Form.Item
+              name="confirm"
+              dependencies={["password"]}
+              hasFeedback
+              rules={[
+                { required: true, message: "Please confirm your password!" },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error("The two passwords do not match!")
+                    );
+                  },
+                }),
+              ]}
+              label="Confirm Password"
+            >
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                placeholder="Confirm Password"
+                className="rounded-lg"
+              />
+            </Form.Item>
+
+            {/* Signup Button */}
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="w-full rounded-lg bg-blue-500 hover:bg-blue-600"
+              >
+                Sign Up
+              </Button>
+            </Form.Item>
+
+            <div className="text-center mt-4">
+              Already have an account?{" "}
+              <Link to="/login" className="text-blue-600">
+                Log in here!
+              </Link>
+            </div>
+          </Form>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Signup;
--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -1,90 +1,82 @@
 import React from "react";
+import { Button, Carousel, Col, Menu, Row } from "antd";
+import { RightOutlined } from "@ant-design/icons";
+import carousel1 from "../../assets/carousel1.png";
+import appleLogo from "../../assets/apple_logo.png";
 
-function Banner() {
+const Banner = () => {
+  const menuItems = [
+    "Woman's Fashion",
+    "Men's Fashion",
+    "Electronics",
+    "Home & Lifestyle",
+    "Medicine",
+    "Sports & Outdoor",
+    "Baby's & Toys",
+    "Groceries & Pets",
+    "Health & Beauty",
+  ];
+
   return (
-    <div className="bg-red-300">
-      <div className="grid grid-cols-4">
-        <div className="hidden md:block">div 1</div>
-        <div className="col-span-4 md:col-span-3">
-          <div className="carousel w-full relative">
-            {/* Carousel items */}
-            <div id="slide1" className="carousel-item relative w-full">
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-                className="w-full"
-              />
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide4" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide2" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-            <div id="slide2" className="carousel-item relative w-full">
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-                className="w-full"
-              />
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide1" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide3" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-            <div id="slide3" className="carousel-item relative w-full">
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-                className="w-full"
-              />
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide2" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide4" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-            <div id="slide4" className="carousel-item relative w-full">
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-                className="w-full"
-              />
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide3" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide1" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
+    <div className="lg:py-10 lg:px-24">
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
+        <div className="hidden lg:block w-64 bg-gray-100 px-4 col-span-1">
+          <Menu mode="vertical" className="bg-gray-100">
+            {menuItems.map((item, index) => (
+              <Menu.Item
+                key={index}
+                className="flex justify-between items-center"
+              >
+                {item}
+                <RightOutlined />
+              </Menu.Item>
+            ))}
+          </Menu>
+        </div>
 
-            {/* Dot buttons (carousel indicators) */}
-            <div className="absolute flex justify-center w-full py-2 bottom-4">
-              <a href="#slide1" className="btn btn-xs btn-circle mx-1">
-                •
-              </a>
-              <a href="#slide2" className="btn btn-xs btn-circle mx-1">
-                •
-              </a>
-              <a href="#slide3" className="btn btn-xs btn-circle mx-1">
-                •
-              </a>
-              <a href="#slide4" className="btn btn-xs btn-circle mx-1">
-                •
-              </a>
+        <div className="bg-black col-span-3">
+          <Carousel autoplay>
+            {/* Slide 1 */}
+            <div className="flex items-center">
+              <Row className="w-full">
+                <Col
+                  span={12}
+                  className="p-3 md:p-5 lg:p-16  content-center justify-center items-center"
+                >
+                  <h2 className="flex items-center">
+                    <img
+                      className="h-6 w-5 lg:h-12 lg:w-10"
+                      src={appleLogo}
+                      alt=""
+                    />{" "}
+                    <span className="font-semibold mt-1 ml-1 md:font-semibold md:text-xl md:mt-2 md:ml-1 lg:font-bold lg:text-2xl lg:mt-4 lg:ml-2 text-white">
+                      iPhone 14 Series
+                    </span>
+                  </h2>
+                  <h1 className="font-semibold text-2xl md:text-3xl mt-3 lg:font-bold lg:text-5xl lg:mt-4 text-white">
+                    Up to 10% off Voucher
+                  </h1>
+                  <Button
+                    type="primary"
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded-full transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg mt-3 lg:mt-6"
+                  >
+                    Shop Now
+                  </Button>
+                </Col>
+                <Col span={12}>
+                  <img
+                    className="py-5 h-[200px] md:h-[300px] lg:h-[400px] w-full object-cover"
+                    src={carousel1}
+                    alt="iPhone"
+                  />
+                </Col>
+              </Row>
             </div>
-          </div>
+          </Carousel>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Banner;
